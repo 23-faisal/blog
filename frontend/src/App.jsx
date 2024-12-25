89844f1e57a7c393066be41ea1ee601a2f9219a8
23-faisal <a.faisal23r@gmail.com>
2024-12-25 09:29:00 +0600

    Create responsive navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
+import Navbar from "./components/Navbar/Navbar";
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
-  )
-}
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-32">
+      {/* Navbar */}
+      <Navbar />
+      {/* Breadcrumb */}
 
-export default App
+      {/* Introduction */}
+
+      {/* Featured Post */}
+      
+      {/* Post List */}
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { IoClose, IoMenu } from "react-icons/io5";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between z-50">
+      {/* Logo */}
+      <div className="flex items-center space-x-2 text-2xl font-bold text-gray-800">
+        <img src="./logo.png" className="w-8 h-8" alt="blog logo" />
+        <span>.blog</span>
+      </div>
+      {/* Mobile Menu */}
+
+      <div className="md:hidden">
+        {isOpen ? (
+          <>
+            <IoClose
+              className="cursor-pointer w-8 h-8 text-red-500 "
+              onClick={() => setIsOpen(!isOpen)}
+            />
+          </>
+        ) : (
+          <>
+            <IoMenu
+              className="cursor-pointer w-8 h-8 text-teal-500 "
+              onClick={() => setIsOpen(!isOpen)}
+            />
+          </>
+        )}
+
+        {/* Mobile Link List */}
+        <div
+          className={`  w-full h-screen flex flex-col items-center gap-8 font-lg justify-center absolute
+         top-16   bg-red-700   ${
+           isOpen ? "-right-0" : "-right-[100%]"
+         }  transition-all duration-500 ease-in-out`}
+        >
+          <a href="">Home</a>
+          <a href="">Most Popular</a>
+          <a href="">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-xl bg-blue-800 text-white">
+              Login
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* Desktop Menu */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="">Home</a>
+        <a href="">Most Popular</a>
+        <a href="">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-xl bg-blue-800 text-white">
+            Login
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
